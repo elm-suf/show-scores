d622ab59bed2e318f71f562609d01790aa2671e8
elm-suf <elm.sufian@gmail.com>
2024-10-18 14:50:42 +0100

    shared library

--- apps/be/src/app/app.service.ts
+++ apps/be/src/app/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Search, TV } from '@shared/models';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  getSeriesByName(term: string): Search<TV> {
+    return {
+      items: [{ id: 1 }],
+    };
   }
 }
---
+++ libs/shared/models/src/lib/models.ts
@@ -0,0 +1,11 @@
+export function models(): string {
+  return 'models';
+}
+
+export type Search<T> = {
+  items: T[];
+};
+
+export type TV = {
+  id: number
+}
