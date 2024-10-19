6646e7e67bf65911cc2fd77494dfc2a54196179a
elm-suf <elm.sufian@gmail.com>
2024-10-19 15:18:10 +0100

    refactor

--- apps/be/src/main.ts
+++ apps/be/src/main.ts
@@ -3,19 +3,13 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-
-  //add SwaggerModule on bootstrap()
+const swaggerConfig = (app: INestApplication) => {
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setVersion('1.0')
@@ -23,11 +17,18 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
-  //end
+};
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+
+  swaggerConfig(app);
   await app.listen(port);
+
   Logger.log(`🔑 api key = ${process.env.TMDB_API_TOKEN}`);
-  
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
